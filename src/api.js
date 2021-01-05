bd48130767b6894ebd9907c652df10238f0822cd
Sikandar Ali <sikandarali4286@gmail.com>
2021-01-05 14:48:58 +0500

    express app for test netlify_api

---
+++ src/api.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        hello: "hi the is a api !!!!!",
+    });
+});
+router.get("/test", (req, res) => {
+    res.json({
+        hello: "Sikandar ALI",
+    });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
